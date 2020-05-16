54756c59985ade28d6e5d984643280c410ad851b
HimanshuYadav <2019kuec2009@iiitkota.ac.in>
2020-05-16 13:17:44 +0530

    Updated UI and updated documentation

--- parallax.js
+++ parallax.js
@@ -10,7 +10,6 @@ window.addEventListener('scroll', () => {
             // If the image is someway below and not seen on the first instance seen of the page that is top of the page, Means we have to scroll the page to find that image.
             if (image[index].height - element.clientHeight > 0) {
                 // normal condition image height is more than the container height.
-                console.log('in if if');
                 var speed =
                     window.screen.availHeight /
                     (image[index].height - element.clientHeight);
@@ -21,12 +20,9 @@ window.addEventListener('scroll', () => {
                         element.offsetTop) /
                     speed
                 }px)`;
-                console.log(`speed of ${index} is ${speed}`);
             } else {
                 const scale_value = element.clientHeight / image[index].height+ 1;
                 // image height is less than the container height so we have to scale the image to overflow the container.
-                console.log('in if elif');
-                console.log(image[index].height - element.clientHeight);
                 var speed =
                     (window.screen.availHeight + element.clientHeight) /
                     (element.clientHeight - image[index].height);
@@ -36,15 +32,12 @@ window.addEventListener('scroll', () => {
                         element.offsetTop) /
                     speed
                 }px) scale(${scale_value})`;
-                console.log(scale_value);
-                console.log(`speed of ${index} is ${speed}`);
             }
         } else if (
             // Image is on the first instance of the page means can be seen without scrolling means image is not so down so that we have to scoll it.
             window.screen.availHeight > element.offsetTop &&
             window.scrollY < element.offsetTop + element.clientHeight
         ) {
-            console.log('Image is present on the first instance page');
             var speed =
                 (element.offsetTop + element.clientHeight) /
                 (image[index].height - element.clientHeight);
