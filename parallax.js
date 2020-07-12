0474b4937499a425aede4c00111252b2926e1c0a
himanshu12345yadav <2019kuec2009@iiitkota.ac.in>
2020-07-12 15:11:03 -0500

    Optimised for small screens

--- parallax.js
+++ parallax.js
@@ -1,51 +1,43 @@
-window.addEventListener('scroll', () => {
-    var image = document.getElementsByTagName('img');
-    var container = document.getElementsByClassName('parallax-container');
-    [...container].forEach((element, index) => {
-        if (
-            window.screen.availHeight < element.offsetTop &&
-            window.scrollY + window.screen.availHeight > element.offsetTop &&
-            window.scrollY < element.offsetTop + element.clientHeight
-        ) {
-            // If the image is someway below and not seen on the first instance seen of the page that is top of the page, Means we have to scroll the page to find that image.
-            if (image[index].height - element.clientHeight > 0) {
-                // normal condition image height is more than the container height.
-                var speed =
-                    window.screen.availHeight /
-                    (image[index].height - element.clientHeight);
+window.addEventListener("scroll", () => {
+  var image = document.getElementsByTagName("img");
+  var container = document.getElementsByClassName("parallax-container");
+  [...container].forEach((element, index) => {
+    if (
+      window.screen.availHeight < element.offsetTop &&
+      window.scrollY + window.screen.availHeight > element.offsetTop &&
+      window.scrollY < element.offsetTop + element.clientHeight
+    ) {
+      // If the image is someway below.
+      if (image[index].height - element.clientHeight > 0) {
+        // Normal condition , image height is more than the container's height.
+        var speed =
+          window.screen.availHeight /
+          (image[index].height - element.clientHeight);
+        image[index].style.transform = `translateY(${
+          (window.scrollY + window.screen.availHeight - element.offsetTop) /
+          speed
+        }px)`;
+      } else {
+        // For images height smaller that the Container's height.
+        const scale_value = element.clientHeight / image[index].height + 1;
+        var speed =
+          (window.screen.availHeight + element.clientHeight) /
+          (element.clientHeight - image[index].height);
 
-                image[index].style.transform = `translateY(${
-                    (window.scrollY +
-                        window.screen.availHeight -
-                        element.offsetTop) /
-                    speed
-                }px)`;
-            } else {
-                const scale_value = element.clientHeight / image[index].height+ 1;
-                // image height is less than the container height so we have to scale the image to overflow the container.
-                var speed =
-                    (window.screen.availHeight + element.clientHeight) /
-                    (element.clientHeight - image[index].height);
-                image[index].style.transform = `translateY(${
-                    (window.scrollY +
-                        window.screen.availHeight -
-                        element.offsetTop) /
-                    speed
-                }px) scale(${scale_value})`;
-            }
-        } else if (
-            // Image is on the first instance of the page means can be seen without scrolling means image is not so down so that we have to scoll it.
-            window.screen.availHeight > element.offsetTop &&
-            window.scrollY < element.offsetTop + element.clientHeight
-        ) {
-            var speed =
-                (element.offsetTop + element.clientHeight) /
-                (image[index].height - element.clientHeight);
-            image[index].style.transform = `translateY(${
-                window.scrollY / speed
-            }px)`;
-        } else {
-            console.log('No image Found');
-        }
-    });
+        image[index].style.transform = `translateY(${
+          (window.scrollY + window.screen.availHeight - element.offsetTop) /
+          speed
+        }px) scale(${scale_value})`;
+      }
+    } else if (
+      // Image can be seen at the first sight when the page is loaded.
+      window.screen.availHeight > element.offsetTop &&
+      window.scrollY < element.offsetTop + element.clientHeight
+    ) {
+      var speed =
+        (element.offsetTop + element.clientHeight) /
+        (image[index].height - element.clientHeight);
+      image[index].style.transform = `translateY(${window.scrollY / speed}px)`;
+    }
+  });
 });
